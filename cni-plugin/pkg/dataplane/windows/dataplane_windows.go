2093f4e479d1f6f8b47daf02b48baf1f573baefb
doctorpangloss <2229300+doctorpangloss@users.noreply.github.com>
2026-03-24 22:42:46 -0700

    Set IPv6:true on dual-stack HNS network (enables NDP on vSwitch)

--- cni-plugin/pkg/dataplane/windows/dataplane_windows.go
+++ cni-plugin/pkg/dataplane/windows/dataplane_windows.go
@@ -601,6 +601,9 @@ func ensureNetworkExistsWithAPI(networkName string, subNet *net.IPNet, subNetV6 
 			"Type":    "L2Bridge",
 			"Subnets": subnets,
 		}
+		if subNetV6 != nil {
+			req["IPv6"] = true
+		}
 
 		reqStr, err := json.Marshal(req)
 		if err != nil {
